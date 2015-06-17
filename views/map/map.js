1e8f0efb85f4df76033e7df6708922632ececc3f
zachmoreno <xachmoreno@gmail.com>
2015-06-17 14:38:56 -0700

    locations in sidebar, locations on the map, geolocation working, and search filtering sidebar all working

--- views/map/map.js
+++ views/map/map.js
@@ -20,23 +20,73 @@ angular.module('yourCoast.map', [])
   var remoteSearchURL = 'http://localhost:7000/access/v1/locations',
 
     locationsAPI   = $resource(remoteSearchURL,
-                            {},
-                            {
-                              getAllLocations: {
-                                method: 'GET',
-                                isArray: true,
-                                cache: true
-                              }
-                            }
-                           );
+	                            {},
+	                            {
+	                              getAllLocations: {
+	                                method: 'GET',
+	                                isArray: true,
+	                                cache: true
+	                              }
+	                            }
+	                           );
 
   return locationsAPI;
 }])
 
 
-.controller('mapController', ['$scope', 'AccessLocationsAPI',
-						function($scope, AccessLocationsAPI) {
+.controller('mapController', ['$scope', 'AccessLocationsAPI', 'uiGmapGoogleMapApi',
+						function($scope, AccessLocationsAPI, uiGmapGoogleMapApi) {
 
-	$scope.locations = AccessLocationsAPI.getAllLocations();
-	console.log($scope.locations);
+	$scope.map = {};
+	$scope.map.locations = [];
+	$scope.map.locations.coords = {};
+	$scope.map.options = {
+							center: {
+								latitude: 37.632711,
+								longitude: -122.572511
+							},
+							zoom: 6,
+							cluster: {
+								minimumClusterSize : 10
+							}
+						};
+	$scope.map.events = {
+							tilesloaded: function (maps, eventName, args) {},
+							dragend: function (maps, eventName, args) {},
+							zoom_changed: function (maps, eventName, args) {}
+                        };
+
+	if("geolocation" in navigator) {
+		$scope.geolocate = function geolocate() {
+			navigator.geolocation.getCurrentPosition(function(position) {
+				$scope.$apply(function() {
+					$scope.map.options.center.latitude = position.coords.latitude;
+					$scope.map.options.center.longitude = position.coords.longitude
+					$scope.map.options.zoom = 12;
+				});
+
+				console.log($scope.map);
+			});
+		}
+	} else {
+		$scope.feedback = "Sorry, your browser doesn't support geolocation."
+	}
+
+	uiGmapGoogleMapApi.then(function(maps) {
+		console.log(maps);
+
+		AccessLocationsAPI.getAllLocations().$promise.then(function(promisedLocations) {
+			$scope.map.locations = promisedLocations;
+
+			angular.forEach(promisedLocations, function(location) {
+				// add coords obj to each location
+				location.coords = {
+					latitude: location.LATITUDE,
+					longitude: location.LONGITUDE
+				};
+			});
+
+			console.log($scope.map.locations);
+		});
+    });
 }]);
--- yourCoast.js
+++ yourCoast.js
@@ -2,5 +2,14 @@
 
 angular.module('yourCoast', ['ngResource',
 							 'ui.router',
+							 'uiGmapgoogle-maps',
 							 'yourCoast.map'
-							]);
+							])
+
+.config(function(uiGmapGoogleMapApiProvider) {
+    uiGmapGoogleMapApiProvider.configure({
+        //    key: 'your api key',
+        v: '3.17',
+        libraries: 'weather,geometry'
+    });
+})
