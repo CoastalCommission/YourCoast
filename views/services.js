b00deb04eb181bef767c099d9bff1c5090b41252
Zachariah Moreno <xachmoreno@gmail.com>
2016-05-17 16:58:26 -0700

    removed previous attempt @ adding Instagram photos

--- views/services.js
+++ views/services.js
@@ -5,7 +5,6 @@
 
     .factory('AccessLocationsAPI', ['$resource', '$stateParams', '$filter', function($resource, $stateParams, $filter) {
         var coastalEndPoint   = 'http://134.186.6.10/access/v1',
-            instagramEndPoint = 'https://api.instagram.com/v1',
 
             locationsAPI   = {
                 getAllLocations: $resource(coastalEndPoint + '/locations/',
@@ -38,17 +37,7 @@
                                                     isArray: true,
                                                     cache: true
                                                 }
-                                           }),
-
-                getPhotosByLatLong: $resource(instagramEndPoint + 'locations/search?lat=' + '&lng=' + '?client_id=27bae1323ac94c67963054afdd50fff4&callback=JSON_CALLBACK',
-                                            {},
-                                            {
-                                                query: {
-                                                    method: 'GET',
-                                                    isArray: true,
-                                                    cache: true
-                                                }
-                                            })
+                                           })
             };
 
         return locationsAPI;
